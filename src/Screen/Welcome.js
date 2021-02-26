c8be5a1ba24efc70b3faac0d56986d9e2501b991
azteck <azteck.indian@gmail.com>
2021-02-26 22:31:11 +0530

    2nd commit

--- src/Screen/Welcome.js
+++ src/Screen/Welcome.js
@@ -4,7 +4,7 @@ import {View, Text} from 'react-native';
 function Welcome(props) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Welcome</Text>
+      <Text>Welcome Aboard</Text>
     </View>
   );
 }
